b13cd9485ba701190caee9333fd5ad8158bfdfc5
Railly <railly_2009@hotmail.com>
2022-12-24 14:51:40 -0500

    fix: add color to ts operators (arithmetic, logical, ternary)

--- demos/ts.ts
+++ demos/ts.ts
@@ -2,6 +2,10 @@
 import { redirect } from '@sveltejs/kit'
 import type { PageLoad } from './$types'
 
+interface Session {
+  user: string
+}
+
 export const load: PageLoad = async ({ parent }) => {
   const { session } = await parent()
   if (!session?.user) {
